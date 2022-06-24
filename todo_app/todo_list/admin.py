8d80d23ed23c132bcf72026654616347e8f9ac4b
thompsongwill <thompsongodswill@gmaill.com>
2022-06-24 12:28:30 +0100

    creating schema

--- todo_app/todo_list/admin.py
+++ todo_app/todo_list/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import List
 
 # Register your models here.
+
+
+admin.site.register(List)
+
+admin.site.site_header = "To Do List App"
+admin.site.site_title = "App Dashboard"
+admin.site.index_title = "To-Do List"
